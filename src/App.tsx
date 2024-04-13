b752b881db9d8c975f61e8a8b04bf7b49008d1ee
Warley <warleysoares35@gmail.com>
2024-04-13 10:41:30 -0300

    shadcn and tailwind add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
     <>
-      <h1>ToSho</h1> 
-      <div>
-        
+      <div className='card'>
+        <h1 className='text-3xl font-bold'>ToSho</h1> 
+        <Button>Saiba mais</Button>
       </div>     
     </>
   )
