8686407f234ac8e2768fe79d2d619d442c91583e
Warley <warleysoares35@gmail.com>
2024-04-15 15:52:28 -0300

    red hat display typography add

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   ],
   prefix: '',
   theme: {
+    fontFamily: {
+      sans: ['Red Hat Display', 'Helvetica', 'Arial', 'sans-serif'],
+    },
     container: {
       center: true,
       padding: '2rem',
