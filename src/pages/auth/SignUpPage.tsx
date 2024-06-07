684be3af961410eaf8838772dbfda17b0739a3ad
Warley <warleysoares35@gmail.com>
2024-06-07 11:31:06 -0300

    Improved validation for strong password on signUp

--- src/pages/auth/SignUpPage.tsx
+++ src/pages/auth/SignUpPage.tsx
@@ -5,7 +5,7 @@ const SignUp = () => {
   const navigate = useNavigate();
 
   return (
-    <div className='flex flex-col justify-center items-center min-w-[320px] min-h-fit space-y-6 bg-secondary p-6 rounded-md border border-accent shadow'>
+    <div className='flex flex-col justify-center items-center w-[320px] min-h-fit space-y-6 bg-secondary p-6 rounded-md border border-accent shadow'>
       <div className='flex flex-col items-center'>
         <h1 className='text-3xl font-black'>ToSho</h1> 
         <span className='max-w-[260px] text-center leading-none font-semibold text-gray-500'>Crie sua conta e comece a organizar suas compras </span>
--- src/utils/form.ts
+++ src/utils/form.ts
@@ -1,5 +1,9 @@
 import { z } from "zod"
 
+// Minimum 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character
+const passwordValidation = new RegExp(
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+);
 
 export const SignUpFormSchema = z.object({
   name: z.string().min(2, {
@@ -8,9 +12,14 @@ export const SignUpFormSchema = z.object({
   email: z.string().email({
     message: "O e-mail digitado está inválido",
   }),
-  password: z.string().min(6, {
-    message: "A senha deve ter pelo menos 6 caracteres.",
-  }),
+  password: z
+    .string()
+    .min(8, {
+      message: "A senha deve ter pelo menos 8 caracteres.",
+    })
+    .regex(passwordValidation, {
+      message: 'A senha deve conter letra maiúscula, letra minúscula, número e caractere especial',
+    }),
   confirmPassword: z.string().min(2, {
     message: "A senha deve ter pelo menos 6 caracteres.",
   }),
