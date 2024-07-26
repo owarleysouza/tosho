724b8ab66cfb54afa940049288686f0b38a901e2
Warley <warleysoares35@gmail.com>
2024-07-26 08:47:36 -0300

    layout of loading page changed

--- src/pages/commom/LoadingPage.tsx
+++ src/pages/commom/LoadingPage.tsx
@@ -1,19 +1,12 @@
-import { Progress } from "@/components/ui/progress"
 import PublicLayout from "@/layouts/PublicLayout"
-import { useEffect, useState } from "react"
+import { LoaderCircle } from "lucide-react"
 
-const LoadingPage = () => {
-  const [progressValue, setProgressValue] = useState(1)
-
-  useEffect(() => {
-    if(progressValue < 100) setProgressValue(progressValue * 10)
-  }, [progressValue])
-
+const LoadingPage = () => { 
   return (
     <PublicLayout>
       <div className="w-1/2 md:w-1/3 pa-0 flex flex-col items-center justify-center space-y-2">
-        <h1 className='text-3xl font-black text-primary'>ToSho...</h1>       
-        <Progress value={progressValue} />
+        <h1 className='text-3xl font-black text-primary'>ToSho</h1>        
+        <LoaderCircle className="text-primary animate-spin" />
       </div>
     </PublicLayout>
   )
