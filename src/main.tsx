a3f031bc39590a8c0570241816dc8eb3e3938022
Warley <warleysoares35@gmail.com>
2024-07-26 08:46:12 -0300

    strict mode removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -6,8 +5,8 @@ import './index.css'
 import { Toaster } from "@/components/ui/toaster"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <>
     <App />
     <Toaster />
-  </React.StrictMode>,
+  </>,
 )
