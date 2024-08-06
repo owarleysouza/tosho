468334dfc22946a8fbe11bf0fbde0f259b12780b
Warley <warleysoares35@gmail.com>
2024-08-06 10:04:12 -0300

    products categorization done

--- src/pages/shop/ProductList.tsx
+++ src/pages/shop/ProductList.tsx
@@ -1,31 +1,61 @@
 import type { Product } from '@/types'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductCard from './ProductCard'
 
 interface ProductListProps {
   products: Product[]
 }
+
+interface CategorizedProductsType{
+  [key: string]: Product[],
+}
+
+interface ProductCategoriesType{
+  [key: string]: string,
+}
  
 const ProductList: React.FC<ProductListProps> = ({products}) => {
 
-  const productCategories = {
+  const [categorizedProducts, setCategorizedProducts] = useState<CategorizedProductsType>({})
+
+  //TODO: Change this with all defined categories on product edit 
+  const productCategories: ProductCategoriesType = {
     others: "Outros",
     cleaning: "Limpeza",
     hygiene: "Higiene Pessoal",
-    Fruits: "Frutas"
+    fruits: "Frutas",
+    bakery: "Padaria"
+  }
+  
+  function categorizeProducts(){
+    const groupedProducts = products.reduce((acc, product) => {
+      if (!acc[product.category]) {
+        acc[product.category] = [];
+      }
+      acc[product.category].push(product);
+      return acc;
+    }, {} as Record<string, Product[]>)
+
+    setCategorizedProducts(groupedProducts)
   }
 
-  //TODO: Change this to a dynamic way
-  //Product Organization Example
-  // const categorizedProducts = 
-  //   {
-  //     others: [{"Product"}]
-  //   }
+  useEffect(() => { 
+    categorizeProducts()
+  }, [products])
   
   return (
-    <div className='h-screen space-y-3'>
-      <h3 className='font-bold'>{productCategories.others}</h3>
-      {products.map((product: Product) => <ProductCard key={product.uid} product={product} />)}
+    <div className='h-screen space-y-4'>
+      {Object.keys(categorizedProducts).map((category) => (
+        <section key={category}>
+          <h3 className='font-bold mb-1'>{productCategories[category]}</h3> 
+          
+          <section className='space-y-2'>
+            {categorizedProducts[category].map((product) => (
+              <ProductCard key={product.uid} product={product} />
+            ))}
+          </section>     
+        </section>
+      ))} 
     </div>
   )
 }
