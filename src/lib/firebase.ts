3d16b074a1a6b8b077ef56a299ab836cca6f9d63
Warley <warleysoares35@gmail.com>
2024-09-02 11:20:44 -0300

    vite variables add

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,10 +5,10 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_API_KEY,
   authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
-  projectId: "tosho-web",
-  storageBucket: "tosho-web.appspot.com",
-  messagingSenderId: "857281222669",
-  appId: "1:857281222669:web:1d2f0b2cfc99d434b41d44"
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APPLICATION_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig)
