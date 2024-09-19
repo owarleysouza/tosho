e5878a7b006fc461363aac62b6f8cbf47f31226b
Warley <warleysoares35@gmail.com>
2024-09-19 09:59:16 -0300

    handle products input improved to accept description

--- src/components/form/ProductFormFooter.tsx
+++ src/components/form/ProductFormFooter.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 import { Button } from '@/components/ui/button';
 import { Form } from '@/components/ui/form';
@@ -8,49 +8,64 @@ import { LoaderCircle } from 'lucide-react';
 
 import { ProductsCreateFormSchema } from '@/utils/formValidations';
 
-import { useForm, UseFormReturn } from "react-hook-form" 
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm, UseFormReturn } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
-interface AddProductsFormProps{
+interface AddProductsFormProps {
   createProductsLoading: boolean;
-  onProductsAdd: (data: z.infer<typeof ProductsCreateFormSchema>, form: UseFormReturn<{text: string;}>) => void;
+  onProductsAdd: (
+    data: z.infer<typeof ProductsCreateFormSchema>,
+    form: UseFormReturn<{ text: string }>
+  ) => void;
 }
 
-const AddProductsForm: React.FC<AddProductsFormProps> =  ({createProductsLoading, onProductsAdd}) => {
-
+const AddProductsForm: React.FC<AddProductsFormProps> = ({
+  createProductsLoading,
+  onProductsAdd,
+}) => {
   const form = useForm<z.infer<typeof ProductsCreateFormSchema>>({
     resolver: zodResolver(ProductsCreateFormSchema),
     defaultValues: {
-      text: "",
+      text: '',
     },
-  })
+  });
 
-  function handleAddProducts(data: z.infer<typeof ProductsCreateFormSchema>){
-    onProductsAdd(data, form)
+  function handleAddProducts(data: z.infer<typeof ProductsCreateFormSchema>) {
+    onProductsAdd(data, form);
   }
 
   return (
-    <footer className='w-80 sticky bottom-0 flex justify-center py-2'>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(handleAddProducts)} className="flex flex-col items-center justify-between w-full space-y-2">
-            <div className="w-full">
-              <FormTextArea
-                formControl={form.control}
-                name="text"
-                placeholder="Digite um ou mais itens a cada linha no formato: Item, Quantidade"
-              />
-            </div>
-            
-            <Button size="sm" disabled={createProductsLoading} type="submit" className='rounded-xl w-full'>
-            { createProductsLoading ? 
-              (<LoaderCircle className="animate-spin" />) 
-              : "Adicionar" }
-            </Button>
-          </form>
-        </Form>
-      </footer>
-  )
-}
+    <footer className="w-80 sticky bottom-0 flex justify-center py-2">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleAddProducts)}
+          className="flex flex-col items-center justify-between w-full space-y-2"
+        >
+          <div className="w-full">
+            <FormTextArea
+              formControl={form.control}
+              name="text"
+              placeholder="Digite um ou mais produtos, um por linha, no formato: Nome, Quantidade, Descrição. Apenas o nome é obrigatório."
+            />
+          </div>
 
-export default AddProductsForm
+          <Button
+            size="sm"
+            disabled={createProductsLoading}
+            type="submit"
+            className="rounded-xl w-full"
+          >
+            {createProductsLoading ? (
+              <LoaderCircle className="animate-spin" />
+            ) : (
+              'Adicionar'
+            )}
+          </Button>
+        </form>
+      </Form>
+    </footer>
+  );
+};
+
+export default AddProductsForm;
--- src/pages/shop/CurrentShopPage.tsx
+++ src/pages/shop/CurrentShopPage.tsx
@@ -106,13 +106,32 @@ const CurrentShopPage: React.FC<ShopProps> = ({ shop }) => {
     const rows = text.trim().split('\n');
 
     for (const row of rows) {
-      const [name, quantity] = row.trim().split(',');
+      const parts = row.trim().split(',');
 
-      //Validate if user just typed \n
-      if (name || quantity) {
+      const name = parts[0]?.trim();
+      let quantity = 1; // Default value for quantity
+      let description = ''; // Default value for description
+
+      // If there's three parts, we assume the second one as quantity and the third as description
+      if (parts.length === 3) {
+        quantity = parseInt(parts[1]) || 1;
+        description = parts[2]?.trim() || '';
+        // If there's just two parts, we verify if the second one is a quantity or a description
+      } else if (parts.length === 2) {
+        //Second one is quantity
+        if (!isNaN(Number(parts[1]))) {
+          quantity = parseInt(parts[1]);
+        } else {
+          description = parts[1]?.trim() || '';
+        }
+      }
+
+      // Name is mandatory
+      if (name) {
         products.push({
-          name: name ? name : 'Produto Exemplo',
-          quantity: quantity ? parseInt(quantity) : 1,
+          name: name,
+          quantity: quantity,
+          description: description,
           category: 'others',
           isDone: false,
         });
