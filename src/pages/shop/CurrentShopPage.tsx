7aaa179b7c4fa09256f6fad4b15a258594a61f98
Warley <warleysoares35@gmail.com>
2024-09-24 11:24:13 -0300

    add products from catalog done #29

--- src/pages/shop/CurrentShopPage.tsx
+++ src/pages/shop/CurrentShopPage.tsx
@@ -10,6 +10,9 @@ import { UserContext } from '@/context/commom/UserContext';
 
 import productsBlankStateSVG from '@/assets/images/products-blank-state.svg';
 import cartBlankStateSVG from '@/assets/images/cart-blank-state.svg';
+import { ListPlus, Loader2 } from 'lucide-react';
+
+import CurrentShopPriceCard from '@/pages/shop/CurrentShopPriceCard';
 
 import BlankState from '@/components/commom/BlankState';
 import ProductFormFooter from '@/components/form/ProductFormFooter';
@@ -17,6 +20,17 @@ import LoadingPage from '@/pages/commom/LoadingPage';
 import { useToast } from '@/components/ui/use-toast';
 import ProductList from '@/components/shop/ProductList';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { Button } from '@/components/ui/button';
+
+import { productsCatalog } from '@/data/productsCatalog';
 
 import { db } from '@/lib/firebase';
 import { addDoc, collection, DocumentData, getDocs } from 'firebase/firestore';
@@ -27,7 +41,7 @@ import {
   setCurrentShopPendingProducts,
   setCurrentShopCartProducts,
 } from '@/app/shop/shopSlice';
-import CurrentShopPriceCard from './CurrentShopPriceCard';
+import { Separator } from '@/components/ui/separator';
 
 interface ShopProps {
   shop: DocumentData; //TODO: Change this type to a Shop type
@@ -35,6 +49,11 @@ interface ShopProps {
 
 const CurrentShopPage: React.FC<ShopProps> = ({ shop }) => {
   const { user } = useContext(UserContext);
+
+  const productsCollectionRef = collection(
+    db,
+    `users/${user?.uid}/shops/${shop.uid}/products`
+  );
 
   const currentShopPendingProducts = useSelector(
     (state: RootState) => state.shop.currentShopPendingProducts
@@ -47,6 +66,12 @@ const CurrentShopPage: React.FC<ShopProps> = ({ shop }) => {
   const [createProductsLoading, setCreateProductsLoading] = useState(false);
 
   const [loadingProducts, setLoadingProducts] = useState(true);
+
+  const [sortedProductsCatalog, setSortedProductsCatalog] = useState<Product[]>(
+    []
+  );
+
+  const [loadingProductCatalogId, setLoadingProductCatalogId] = useState('');
 
   const { toast } = useToast();
 
@@ -160,13 +185,8 @@ const CurrentShopPage: React.FC<ShopProps> = ({ shop }) => {
     try {
       setCreateProductsLoading(true);
       if (user) {
-        const productsCollRef = collection(
-          db,
-          `users/${user.uid}/shops/${shop.uid}/products`
-        );
-
         const productPromises = productsToAdd.map(async (product) => {
-          const { id } = await addDoc(productsCollRef, product);
+          const { id } = await addDoc(productsCollectionRef, product);
           const newProduct = {
             uid: id,
             ...product,
@@ -194,20 +214,106 @@ const CurrentShopPage: React.FC<ShopProps> = ({ shop }) => {
     }
   }
 
+  function sortProductsCatalog() {
+    setSortedProductsCatalog(
+      productsCatalog.sort((a, b) => a.name.localeCompare(b.name))
+    );
+  }
+
+  async function addProductFromCatalog(product: Product) {
+    const productToAdd = {
+      name: product.name,
+      quantity: product.quantity,
+      category: product.category,
+      isDone: false,
+    };
+
+    try {
+      setLoadingProductCatalogId(product.uid);
+      if (user) {
+        const { id } = await addDoc(productsCollectionRef, productToAdd);
+        const newProduct = {
+          ...product,
+          uid: id,
+        };
+
+        dispatch(
+          setCurrentShopPendingProducts(
+            currentShopPendingProducts.concat(newProduct)
+          )
+        );
+
+        toast({
+          variant: 'success',
+          title: 'Produto adicionado',
+        });
+      }
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        title: 'Ops! Algo de errado aconteceu',
+        description: 'Um erro inesperado aconteceu na adição do produto',
+      });
+    } finally {
+      setLoadingProductCatalogId('');
+    }
+  }
+
   useEffect(() => {
     getProducts();
+    sortProductsCatalog();
   }, []);
 
   if (loadingProducts) return <LoadingPage />;
 
   return (
     <div className="mt-16">
-      <section className="flex flex-col items-center my-3">
-        <span className="text-xs text-slate-400">
-          {formatDate(shop.date?.seconds)}
-        </span>
-        <span className="text-lg text-black font-bold">{shop.name}</span>
-      </section>
+      <div className="flex flex-row items-center justify-between">
+        <section className="flex flex-col items-center my-3 mx-auto">
+          <span className="text-xs text-slate-400">
+            {formatDate(shop.date?.seconds)}
+          </span>
+          <span className="text-lg text-black font-bold">{shop.name}</span>
+        </section>
+        <Sheet>
+          <SheetTrigger>
+            <ListPlus className="cursor-pointer" />
+          </SheetTrigger>
+          <SheetContent className="w-full overflow-y-scroll">
+            <SheetHeader>
+              <SheetTitle>Catálogo de Produtos</SheetTitle>
+              <SheetDescription>
+                Adicione um ou mais produtos para sua compra atual de forma
+                simples e rápida
+              </SheetDescription>
+
+              <section className="py-2">
+                {sortedProductsCatalog.map((product) => (
+                  <div key={product.uid}>
+                    <div className="flex flex-row items-center justify-between">
+                      <span>{product.name}</span>
+                      <Button
+                        disabled={loadingProductCatalogId === product.uid}
+                        type="button"
+                        onClick={() => addProductFromCatalog(product)}
+                        className="rounded-full px-4"
+                        variant="ghost"
+                      >
+                        {loadingProductCatalogId === product.uid ? (
+                          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                        ) : (
+                          'Adicionar'
+                        )}
+                      </Button>
+                    </div>
+                    <Separator className="my-1" />
+                  </div>
+                ))}
+              </section>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
 
       <Tabs defaultValue="list" value={activeTab} onValueChange={setActiveTab}>
         <TabsList className="w-80 bg-secondary">
