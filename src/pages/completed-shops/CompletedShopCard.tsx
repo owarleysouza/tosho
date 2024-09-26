fb69bcaa6c704cd235c2ec041e6a402087b226f6
Warley <warleysoares35@gmail.com>
2024-09-26 09:47:31 -0300

    badge on completed shop card to indicate shop conclusion done

--- src/pages/completed-shops/CompletedShopCard.tsx
+++ src/pages/completed-shops/CompletedShopCard.tsx
@@ -47,51 +47,54 @@ const ShopCard: React.FC<MyShopsProps> = ({
   }
 
   return (
-    <div className="flex flex-row w-[316px] min-h-[62px] justify-between bg-secondary py-3 px-4 rounded-2xl border border-accent shadow gap-2">
-      <section
-        className="cursor-pointer flex flex-row items-center gap-2"
-        onClick={goToShopDetail}
-      >
-        <ShoppingBasket />
-        <div className="flex flex-col">
-          <span className="text-xs text-black font-semibold break-all ...">
-            {shop.name}
-          </span>
-          <span className="text-xs text-slate-400 break-all ...">
-            {formatDate(shop.date?.seconds)}
-          </span>
-        </div>
-      </section>
+    <div className="relative ">
+      <div className="absolute t-0 left-0 w-[12px] min-h-full bg-primary rounded-tl-full rounded-bl-full border"></div>
+      <div className="flex flex-row w-[316px] min-h-[62px] justify-between bg-secondary py-3 pl-6 pr-4 rounded-2xl border border-accent shadow gap-2">
+        <section
+          className="cursor-pointer flex flex-row items-center gap-2"
+          onClick={goToShopDetail}
+        >
+          <ShoppingBasket />
+          <div className="flex flex-col">
+            <span className="max-w-[140px] text-xs text-black font-semibold break-all ...">
+              {shop.name}
+            </span>
+            <span className="text-xs text-slate-400 break-all ...">
+              {formatDate(shop.date?.seconds)}
+            </span>
+          </div>
+        </section>
 
-      <section className="flex flex-row items-center gap-1.5">
-        <span className="text-xs text-black font-bold">
-          {formatPrice(shop.total)}
-        </span>
-        <DropdownMenu open={openMenu} onOpenChange={setOpenMenu}>
-          <DropdownMenuTrigger asChild>
-            <EllipsisVertical className="h-5 w-5 cursor-pointer" />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem
-              className="cursor-pointer"
-              onClick={onOpenRemoveDialog}
-            >
-              Excluir
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </section>
+        <section className="flex flex-row items-center gap-1.5">
+          <span className="text-xs text-black font-bold">
+            {formatPrice(shop.total)}
+          </span>
+          <DropdownMenu open={openMenu} onOpenChange={setOpenMenu}>
+            <DropdownMenuTrigger asChild>
+              <EllipsisVertical className="h-5 w-5 cursor-pointer" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem
+                className="cursor-pointer"
+                onClick={onOpenRemoveDialog}
+              >
+                Excluir
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </section>
 
-      <DecisionDialog
-        title="Excluir compra?"
-        description="Todos os dados dessa compra serão perdidos e esta ação não poderá ser desfeita."
-        actionLabel="Excluir"
-        type="danger"
-        open={openRemoveDialog}
-        setOpen={setOpenRemoveDialog}
-        loading={removeShopLoading}
-        onConfirm={removeShop}
-      />
+        <DecisionDialog
+          title="Excluir compra?"
+          description="Todos os dados dessa compra serão perdidos e esta ação não poderá ser desfeita."
+          actionLabel="Excluir"
+          type="danger"
+          open={openRemoveDialog}
+          setOpen={setOpenRemoveDialog}
+          loading={removeShopLoading}
+          onConfirm={removeShop}
+        />
+      </div>
     </div>
   );
 };
