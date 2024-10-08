26aa4e1dbf1cb48247fe9a23e97f2fd2a231bad1
Warley <warleysoares35@gmail.com>
2024-10-08 08:32:00 -0300

    function to handle products input moved to utils

--- src/pages/shop/CurrentShopPage.tsx
+++ src/pages/shop/CurrentShopPage.tsx
@@ -6,6 +6,8 @@ import { Product } from '@/types';
 import { ProductsCreateFormSchema } from '@/utils/formValidations';
 import { formatDate } from '@/utils/formatDate';
 
+import { handleProductsInput } from '@/utils/handleProductsInput';
+
 import { UserContext } from '@/context/commom/UserContext';
 
 import productsBlankStateSVG from '@/assets/images/products-blank-state.svg';
@@ -124,46 +126,6 @@ const CurrentShopPage: React.FC<ShopProps> = ({ shop }) => {
     } finally {
       setLoadingProducts(false);
     }
-  }
-
-  function handleProductsInput(text: string) {
-    const products = [];
-    const rows = text.trim().split('\n');
-
-    for (const row of rows) {
-      const parts = row.trim().split(',');
-
-      const name = parts[0]?.trim();
-      let quantity = 1; // Default value for quantity
-      let description = ''; // Default value for description
-
-      // If there's three parts, we assume the second one as quantity and the third as description
-      if (parts.length === 3) {
-        quantity = parseInt(parts[1]) || 1;
-        description = parts[2]?.trim() || '';
-        // If there's just two parts, we verify if the second one is a quantity or a description
-      } else if (parts.length === 2) {
-        //Second one is quantity
-        if (!isNaN(Number(parts[1]))) {
-          quantity = parseInt(parts[1]);
-        } else {
-          description = parts[1]?.trim() || '';
-        }
-      }
-
-      // Name is mandatory
-      if (name) {
-        products.push({
-          name: name,
-          quantity: quantity,
-          description: description,
-          category: 'others',
-          isDone: false,
-        });
-      }
-    }
-
-    return products;
   }
 
   async function onSubmitProduct(
---
+++ src/utils/handleProductsInput.ts
@@ -0,0 +1,39 @@
+export function handleProductsInput(text: string) {
+  const products = [];
+  const rows = text.trim().split('\n');
+
+  for (const row of rows) {
+    const parts = row.trim().split(',');
+
+    const name = parts[0]?.trim();
+    let quantity = 1; // Default value for quantity
+    let description = ''; // Default value for description
+
+    // If there's three parts, we assume the second one as quantity and the third as description
+    if (parts.length === 3) {
+      quantity = parseInt(parts[1]) || 1;
+      description = parts[2]?.trim() || '';
+      // If there's just two parts, we verify if the second one is a quantity or a description
+    } else if (parts.length === 2) {
+      //Second one is quantity
+      if (!isNaN(Number(parts[1]))) {
+        quantity = parseInt(parts[1]);
+      } else {
+        description = parts[1]?.trim() || '';
+      }
+    }
+
+    // Name is mandatory
+    if (name) {
+      products.push({
+        name: name,
+        quantity: quantity,
+        description: description,
+        category: 'others',
+        isDone: false,
+      });
+    }
+  }
+
+  return products;
+}
